852e90dd1d28e507ab7c3f7493f011c832035f3d
NathanielWatife <awosanminathaniel0@gmail.com>
2024-12-06 19:34:33 +0100

    Added blog, comments, post router

--- Routes/userRoute.js
+++ Routes/userRoute.js
@@ -1,11 +1,27 @@
 const express = require('express');
-const { signUp, getAllUsers } = require('../Controller/userController');
 const router = express.Router();
+const { signUp, getAllUsers, updateUserProfile, viewUserProfile } = require('../Controller/userController');
+const { createPost, getPosts, updatePost, deletePost } = require('../Controller/postController');
+const { addComment, editComment, deleteComment } = require('../Controller/commentController');
+const { authenticate, authorize } = require('../middleware/authMiddleware');
 
-// Route to register a new user
+// Route for users
 router.post('/register', signUp);
+router.get('/users', authenticate, authorize('Admin'), getAllUsers);
+router.put('/profile', authenticate, updateUserProfile);
+router.get('/profile/:userId', authenticate, viewUserProfile);
 
-// Route to fetch all users
-router.get('/users', getAllUsers);
+// Route for blogs
+router.post('/posts', authenticate, authorize('Author', 'Admin'), createPost);
+router.get('/posts', getPosts);
+router.put('/posts/:postId', authenticate, authorize('Author', 'Admin'), updatePost);
+router.delete('/posts/:postId', authenticate, authorize('Author', 'Admin'), deletePost)
+
+
+// comments routes
+router.post('/posts/:postId/comments', authenticate, addComment);
+router.put('/comments/commentId', authenticate, authorize('Admin'), editComment);
+router.delete('/comments/commentId', authenticate, authorize('Admin'), deleteComment)
+
 
 module.exports = router;
